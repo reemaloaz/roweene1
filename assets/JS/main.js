e3b8a352d9191c37de6db819906478c9cac2ff1e
ssha013 <63234985+ssha013@users.noreply.github.com>
2023-04-30 11:58:42 +0300

    slider images

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -7,50 +7,111 @@ $(document).ready(function () {
     dots: true,
   });
 });
-$(".slider-for").slick({
-  rtl: true,
+// $(".slider-for").slick({
+//   rtl: true,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   fade: true,
+//   asNavFor: ".slider-nav",
+// });
+// $(".slider").slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   asNavFor: ".slider-for",
+//   dots: true,
+//   centerMode: true,
+//   focusOnSelect: true,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         infinite: true,
+//         dots: true,
+//       },
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     // You can unslick at a given breakpoint now by adding:
+//     // settings: "unslick"
+//     // instead of a settings object
+//   ],
+// });
+
+
+
+$('.slider-single').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: ".slider-nav",
+  arrows: true,
+  rtl:true,
+  fade: false,
+  adaptiveHeight: true,
+  infinite: false,
+  useTransform: true,
+  speed: 400,
+  cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
 });
-$(".slider").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: ".slider-for",
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ],
+
+$('.slider-nav')
+    .on('init', function(event, slick) {
+      $('.slider-nav .slick-slide.slick-current').addClass('is-active');
+    })
+    .slick({
+      slidesToShow: 7,
+      slidesToScroll: 7,
+      dots: false,
+      focusOnSelect: false,
+      infinite: false,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      }, {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      }, {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      }]
+    });
+
+$('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+  $('.slider-nav').slick('slickGoTo', currentSlide);
+  var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+  $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+  $(currrentNavSlideElem).addClass('is-active');
 });
+
+$('.slider-nav').on('click', '.slick-slide', function(event) {
+  event.preventDefault();
+  var goToSingleSlide = $(this).data('slick-index');
+
+  $('.slider-single').slick('slickGoTo', goToSingleSlide);
+});
+
 const allBoxes = document.querySelector(".all-boxes");
 const messageLink = document.querySelector(".message-link");
 
